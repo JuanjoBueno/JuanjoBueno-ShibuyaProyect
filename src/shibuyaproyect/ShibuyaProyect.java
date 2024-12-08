dd2fc8184d8570b03748bb5a1904863278ce729e
JuanjoBueno <jbaprogramacion@gmail.com>
2024-12-08 02:30:56 +0100

    busqueda de excepciones

--- src/shibuyaproyect/ShibuyaProyect.java
+++ src/shibuyaproyect/ShibuyaProyect.java
@@ -1,40 +1,45 @@
-
 package shibuyaproyect;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class ShibuyaProyect {
 
-   
     public static void main(String[] args) {
-        
+
         Garaje n = new Garaje("n");
-        Garaje s  = new Garaje("s");
-        Garaje e  = new Garaje("e");
-        Garaje w  = new Garaje("w");
-        
+        Garaje s = new Garaje("s");
+        Garaje e = new Garaje("e");
+        Garaje w = new Garaje("w");
+
         Map<String, Garaje> garajes = new HashMap<>();
         garajes.put("N", n);
         garajes.put("S", s);
         garajes.put("E", e);
         garajes.put("W", w);
 
-        Semaforo s1 = new Semaforo("s1", garajes, false);
-        Semaforo s2 = new Semaforo("s2", garajes, true);
-        
-        n.setS1(s1);
-        n.setS2(s2);
-        s.setS1(s1);
-        s.setS2(s2);
-        e.setS1(s1);
-        e.setS2(s2);
-        e.getS2().setIsVerde(false);
-        w.setS1(s1);
-        w.setS2(s2);
-        w.getS2().setIsVerde(false);
-        
+        new Thread(n).start();
+        new Thread(s).start();
+        new Thread(e).start();
+        new Thread(w).start();
+
+        n.setS1(new Semaforo("s1", garajes, false));
+        n.setS2(new Semaforo("s2", garajes, true));
+        s.setS1(new Semaforo("s1", garajes, false));
+        s.setS2(new Semaforo("s2", garajes, true));
+        e.setS1(new Semaforo("s1", garajes, false));
+        e.setS2(new Semaforo("s1", garajes, false));
+        w.setS1(new Semaforo("s1", garajes, false));
+        w.setS2(new Semaforo("s1", garajes, false));
+
+        new Thread(n.getS1()).start();
+        new Thread(n.getS2()).start();
+        new Thread(s.getS1()).start();
+        new Thread(s.getS2()).start();
+        new Thread(e.getS1()).start();
+        new Thread(e.getS2()).start();
+        new Thread(w.getS1()).start();
+        new Thread(w.getS2()).start();
     }
-    
+
 }
